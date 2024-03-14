705316ca5d52f9de110b90ba6935048785803bcd
Benjamin Walkenhorst <krylon@posteo.de>
2024-03-14 18:42:04 +0100

    Add boilerplate files

---
+++ __init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Time-stamp: <2024-03-14 18:42:26 krylon>
+#
+# /data/code/python/cephalopod/__init__.py
+# created on 14. 03. 2024
+# (c) 2024 Benjamin Walkenhorst
+#
+# This file is part of the Wetterfrosch weather app. It is distributed
+# under the terms of the GNU General Public License 3. See the file
+# LICENSE for details or find a copy online at
+# https://www.gnu.org/licenses/gpl-3.0
+
+"""
+cephalopod.__init__
+
+(c) 2024 Benjamin Walkenhorst
+"""
+
+# Local Variables: #
+# python-indent: 4 #
+# End: #
